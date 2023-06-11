90060204408fae29c581f9128b543f774a49dff1
KDamir0 <damirkoshkinbaev1@gmail.com>
2023-06-11 16:56:16 +0600

    Initial commit

---
+++ damir/src/main/java/com/java/damir/DamirApplication.java
@@ -0,0 +1,13 @@
+package com.java.damir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamirApplication.class, args);
+	}
+
+}
---
+++ damir/src/test/java/com/java/damir/DamirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.damir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
